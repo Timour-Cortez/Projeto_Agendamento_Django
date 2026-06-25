96932ff0d81314628c10f3ae8f96e95a7b1b0e72
Y-R-S-S <pimbalovisk@gmail.com>
2026-06-25 20:07:33 -0300

    Register PlanoAssinatura and Assinatura in admin

--- agendamento/admin.py
+++ agendamento/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Cliente, Servico, LocalAtendimento, Agendamento
+from .models import Cliente, Servico, LocalAtendimento, Agendamento, PlanoAssinatura, Assinatura
 
+admin.site.register(PlanoAssinatura)
+admin.site.register(Assinatura)
 
 admin.site.register(Cliente)
 admin.site.register(Servico)
