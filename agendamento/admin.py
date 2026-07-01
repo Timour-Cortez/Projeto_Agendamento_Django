939d182a0cd76e3bd3f1ed25f96440db07e2174f
Y-R-S-S <pimbalovisk@gmail.com>
2026-07-01 14:29:19 -0300

    Add Prestador model to admin registration

--- agendamento/admin.py
+++ agendamento/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Cliente, Servico, LocalAtendimento, Agendamento, PlanoAssinatura, Assinatura
+from .models import Cliente, Servico, LocalAtendimento, Agendamento, PlanoAssinatura, Assinatura, Prestador
 
 admin.site.register(PlanoAssinatura)
 admin.site.register(Assinatura)
